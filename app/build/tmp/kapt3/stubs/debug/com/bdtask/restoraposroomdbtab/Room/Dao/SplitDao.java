26e0cf432228ab8cf3caba4b419cfc96bd8365ec
daudhrimon <daudhrimon@gmail.com>
2022-08-31 18:03:06 +0600

    fixed

--- app/build/tmp/kapt3/stubs/debug/com/bdtask/restoraposroomdbtab/Room/Dao/SplitDao.java
+++
@@ -1,7 +0,0 @@
-package com.bdtask.restoraposroomdbtab.Room.Dao;
-
-import java.lang.System;
-
-@kotlin.Metadata(mv = {1, 6, 0}, k = 1, d1 = {"\u0000\n\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0000\bf\u0018\u00002\u00020\u0001\u00a8\u0006\u0002"}, d2 = {"Lcom/bdtask/restoraposroomdbtab/Room/Dao/SplitDao;", "", "app_debug"})
-public abstract interface SplitDao {
-}
