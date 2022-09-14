1d3fae5c0bdc4c9571e994bca415b8360c149a23
daudhrimon <daudhrimon@gmail.com>
2022-09-14 11:05:18 +0600

    about us done

--- app/src/main/java/com/bdtask/restoraposlite/Fragment/AboutUsFragment.kt
+++ app/src/main/java/com/bdtask/restoraposlite/Fragment/AboutUsFragment.kt
@@ -1,12 +1,21 @@
 package com.bdtask.restoraposlite.Fragment
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.bdtask.restoraposlite.databinding.FragmentAboutusBinding
+import es.dmoral.toasty.Toasty
+import java.lang.Exception
+
 
 class AboutUsFragment : Fragment() {
     private lateinit var binding: FragmentAboutusBinding
@@ -18,9 +27,51 @@ class AboutUsFragment : Fragment() {
         binding = FragmentAboutusBinding.inflate(inflater, container, false)
 
 
+
         binding.back.setOnClickListener {
             findNavController().popBackStack()
         }
+
+
+
+        binding.email.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SENDTO)
+            intent.data = Uri.parse("mailto:info@restorapos.com")
+            if (intent.resolveActivity(requireActivity().packageManager) != null) {
+                requireActivity().startActivity(intent)
+            } else {
+                Toasty.info(requireContext(),"No Email Sender App found in your device",Toasty.LENGTH_SHORT).show()
+            }
+        }
+
+
+
+        binding.phone.setOnClickListener {
+            try {
+                if (ContextCompat.checkSelfPermission(requireActivity(),Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED) {
+                    val callIntent = Intent(Intent.ACTION_CALL)
+                    callIntent.data = Uri.parse("tel:+8801531987110")
+                    requireActivity().startActivity(callIntent)
+                } else {
+                    ActivityCompat.requestPermissions(requireActivity(), arrayOf(Manifest.permission.CALL_PHONE), 2)
+                }
+            } catch (e: Exception){
+                Toasty.info(requireContext(),"No Phone App found in your device",Toasty.LENGTH_SHORT).show()
+            }
+
+        }
+
+
+
+        binding.webSite.setOnClickListener {
+            try {
+                val intent = Intent(Intent.ACTION_VIEW,Uri.parse("http://restorapos.com"))
+                requireActivity().startActivity(intent)
+            } catch (e: Exception){
+                Toasty.info(requireContext(),"No Browser App found in your device",Toasty.LENGTH_SHORT).show()
+            }
+        }
+
 
 
         return binding.root
